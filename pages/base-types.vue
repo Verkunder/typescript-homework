8901bd79dac83ac2202317592deba16cc3f6dfbe
Verkunder <Verkunder614@gmail.com>
2022-08-16 22:27:06 +0300

    feat(pages): добавил вторую страницу

--- pages/base-types.vue
+++ pages/base-types.vue
@@ -1,18 +1,18 @@
 <template>
     <div>
         <h1 class="text-center text-4xl font-bold mt-10">
-            Typescript - конфиг
+            Базовые типы
         </h1>
-        <AlwaysStrict />
-        <BaseUrl />
-        <Target />
-        <Alias />
-        <Include />
-        <Exclude />
-        <TypeRoots />
-
         <div class="flex items-center justify-center mb-5">
             <div class="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
+                <div class="rounded-md shadow">
+                    <NuxtLink to='/'
+                        class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10">
+                        Назад
+                    </NuxtLink>
+                </div>
+            </div>
+            <div class="mt-5 ml-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
                 <div class="rounded-md shadow">
                     <NuxtLink to='/base-types'
                         class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10">
@@ -25,17 +25,9 @@
 </template>
 
 <script setup>
-import BaseUrl from "../components/config/BaseUrl.vue";
-import AlwaysStrict from "../components/config/AlwaysStrict.vue";
-import Target from "../components/config/Target.vue";
-import Alias from "../components/config/Alias.vue";
-import Include from "../components/config/Include.vue";
-import Exclude from "../components/config/Exclude.vue";
-import TypeRoots from "../components/config/TypeRoots.vue";
 useHead( {
-    titleTemplate: 'Typescript - конфиг'
+    titleTemplate: 'Typescript - Базовые типы'
 } )
-
 </script>
 
 <style lang="scss" scoped>
